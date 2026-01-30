9fccc3f8b4d07db1affc5ab3975d3eabce938be9
Paulo Sergio R F Tenorio <paulo.tenorio@anota.ai>
2026-01-30 13:57:31 -0300

    feat: enhance message handling to track user message history and respond accordingly

--- src/app/modules/discord/index.js
+++ src/app/modules/discord/index.js
@@ -1,5 +1,14 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 
+const messages = [
+    {
+        author: 'Tag of Author, avoid store the bot messages',
+        history: [
+            { msg: 'The entire message object', content: 'The message content from this user!' }
+        ]
+    }
+]
+
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
@@ -10,9 +19,33 @@ client.on('ready', () => {
 
 client.on('messageCreate', (msg) => {
     console.log('Message: ', msg.content);
-    if (msg.author != client.user.tag) {
-        msg.reply('Hello!')
-        msg.channel.send('How are you?')
+    console.log('Author', msg.author);
+    console.log('User: ', client.user);
+    if (msg.author.tag != client.user.tag) {
+        let historyMessages = messages.find(message => message.author === msg.author.tag);
+
+        if (!historyMessages) {
+            const initialMessages = {
+                author: msg.author.tag,
+                history: [
+                    {msg: msg, content: msg.content}
+                ]
+            }
+            messages.push(initialMessages); 
+        } else {
+            historyMessages.history.push({msg: msg, content: msg.content})
+        }
+
+        historyMessages = messages.find(message => message.author === msg.author.tag);
+
+        console.log('Mensages desse Author', historyMessages)
+        
+        if(historyMessages.history.length === 1) {
+            return msg.reply(`Hello ${msg.author.username}, what you want to do?`);
+        } else  {
+            msg.channel.send('Initiating your process')
+        }
+
     }
 })
 
