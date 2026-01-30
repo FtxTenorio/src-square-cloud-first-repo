f3452346f42662f49c21f6e7606b2cf136b9ef51
Paulo Sergio R F Tenorio <paulo.tenorio@anota.ai>
2026-01-29 21:39:06 -0300

    feat: add POST route to handle name submission with validation

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,6 +3,14 @@ function routes(fastify, options) {
         console.log('Hello world')
         return { hello: 'world' }
     })
+    
+    fastify.post('/', async (req, res) => {
+        if(req.body?.name) {
+            return {response: req.body.name}
+        } else {
+            return {error: 'No Valid Body Was Sent!'}
+        }
+    })
 }
 
 export default routes;
