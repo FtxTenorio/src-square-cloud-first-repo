8cfd45f412b7430bb091438f83d5df9fef580df9
FtxDante <blog.anutrof@gmail.com>
2026-02-21 19:10:17 +0000

    chore(next.config.ts): Remove unused Next.js configuration file
    - Deleted the `next.config.ts` file as it was no longer needed for the project setup.

---
+++ src/client/next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* opções adicionais aqui */
+};
+
+export default nextConfig;
--- src/client/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* opções adicionais aqui */
-};
-
-export default nextConfig;
